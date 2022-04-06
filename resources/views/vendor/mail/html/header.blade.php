7f9d7f4d3a44c0e645369cb62bb192f4308993ba
g0enitz <soulofshaka@gmail.com>
2022-04-06 11:30:15 -0500

    logo mail

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,8 @@
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
 <!-- <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo"> -->
-<img src="{{asset('/logos/notification-logo.png')}}" class="logo" alt="Laravel Logo">
+<img src="https://png.pngtree.com/png-clipart/20190705/original/pngtree-wedding-logo-in-vintage-style-png-image_4359658.jpg" class="logo" alt="Laravel Logo">
+
 @else
 {{ $slot }}
 @endif
