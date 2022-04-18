23f324ff622b4074db13145af487c061b656b844
g0enitz <soulofshaka@gmail.com>
2022-04-18 13:09:10 -0500

    cambios varios4

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="https://wedding-solvit.com/#/" style="display: inline-block;">
 <!-- @if (trim($slot) === "Boda Caro y Dani") -->
-<img src="https://by3302files.storage.live.com/y4miKr165xfnpJ3071Vd1hOyOU-3IncEoyDmkoBEITFw25j4DGeyG5M5qia8IdjrLQh71dgUnI6y4olZUOE5di--CjiGcB6JYzjhtDmJWI_a0kxEM2Ot7-NyLpdD4hi1H51r8sunR8JpfLBu5ZG7ws2sfOC2CgoYkxHyYpm25oC5-IKnKev-87EQ7LATlwrrUZsp0G_Nb92ESCUoMp7eVCNcw/DSC02546%20-MAIL650.jpg?psid=1&width=650&height=433"
+<img src="https://backend.wedding-solvit.com/assets/images/beTnzLh7691650305100.jpg"
  class="logo" alt="" style="width:100px; height:auto">
 <!-- @else
 {{ $slot }}
